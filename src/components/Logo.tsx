7db92511d3814dc6dcc73f9e372c133185e4a181
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-27 20:41:06 +0000

    كبرو كمان تاني

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,8 +5,8 @@ import { ShoppingBag } from 'lucide-react';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <ShoppingBag className="h-8 w-8 text-primary" />
-      <span className="text-2xl font-bold font-headline tracking-tight">
+      <ShoppingBag className="h-10 w-10 text-primary" />
+      <span className="text-3xl font-bold font-headline tracking-tight">
         Do<span className="text-primary">m</span>a
       </span>
     </Link>
