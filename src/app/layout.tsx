a6b1ba5e3962584c76cebb13819a44a53cb869c0
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-28 21:53:43 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { CartProvider } from '@/context/CartContext';
-import { body, html } from 'next/dist/client/components/react-dev-overlay/internal/components/Terminal/Terminal';
 
 export const metadata: Metadata = {
   title: 'Doma',
@@ -27,13 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar" dir="rtl" suppressHydrationWarning>
+    <html lang="ar" dir="rtl">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body suppressHydrationWarning={true}>
         <CartProvider>
           {children}
           <Toaster />
