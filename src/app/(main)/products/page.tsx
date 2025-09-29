b9d96fdb017c6d2e6f0708b404a56d63858486f5
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-29 20:45:12 +0000

    عايز لما الشاشه تسغر يعمي اسكرول يمين وشمال والكارت يفضل حجمو ثابت

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -119,7 +119,7 @@ export default function ProductsPage() {
                 ) : (
                     <>
                         {filteredProducts.length > 0 ? (
-                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                        <div className="flex flex-wrap gap-4">
                             {filteredProducts.map((product) => (
                                 <div key={product.id}>
                                   <ProductCard product={product} />
