fa09e2c8c86b82313bd754d87c187aaca4625ae0
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-29 19:29:43 +0000

    اعملو   بخط سميك

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
       <ShoppingBag className="h-12 w-12 text-primary" strokeWidth={2.5} />
-      <span className="text-4xl font-extrabold font-headline tracking-tight">
+      <span className="text-4xl font-black font-headline tracking-tight">
         Do<span className="text-primary">m</span>a
       </span>
     </Link>
