6455c3757603f12502a3bdd889a001d05eff209c
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-29 23:01:25 +0000

    طيب خلي حجم الكارت ثابت ويكون حجم واحد على كل الشاشات   ميتغيرش ابدا

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -145,9 +145,9 @@ export default function ProductsPage() {
                   </form>
                 </div>
                  {loading ? (
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                    <div className="flex flex-wrap gap-4">
                         {[...Array(12)].map((_, j) => (
-                             <div key={j} className="flex justify-center">
+                             <div key={j}>
                                 <Skeleton className="h-96 w-[180px]" />
                             </div>
                         ))}
@@ -155,9 +155,9 @@ export default function ProductsPage() {
                 ) : (
                     <>
                         {filteredProducts.length > 0 ? (
-                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                        <div className="flex flex-wrap gap-4">
                             {filteredProducts.map((product) => (
-                                <div key={product.id} className="flex justify-center">
+                                <div key={product.id}>
                                   <ProductCard product={product} />
                                 </div>
                             ))}
